a805f2a9a8ca3c3af8aed7683ed56c0b7c427c68
Srijan Khadka <98076207+SriZan12@users.noreply.github.com>
2026-04-24 21:54:56 +0545

    Move MainActivity nav host container to Compose shell

--- app/src/main/java/com/example/limitr/ui/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/limitr/ui/mainactivity/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.limitr.ui.mainactivity
 
 import android.os.Bundle
-import android.view.View
+import android.view.ViewGroup
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.limitr.R
-import com.example.limitr.databinding.ActivityMainBinding
+import com.example.limitr.ui.theme.LimitrTheme
 import com.example.limitr.utils.Constants.OVERLAY_DISPLAYED
 import com.example.limitr.utils.OverlayScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,9 +29,8 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var activityMainBinding: ActivityMainBinding
     private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    private var showToolbar by mutableStateOf(true)
 
     @Inject
     lateinit var overlayScreen: OverlayScreen
@@ -30,41 +38,72 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         overlayScreen.removeOverlayView()
-        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        setContentView(activityMainBinding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.findNavController()
+        setContent {
+            LimitrTheme {
+                MainActivityScreen()
+            }
+        }
+    }
 
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.fragmentSignup,
-                R.id.fragmentHome
-            )
-        )
+    @Composable
+    private fun MainActivityScreen() {
+        Column(modifier = Modifier.fillMaxSize()) {
+            if (showToolbar) {
+                TopAppBar(
+                    title = { Text(text = getString(R.string.app_name)) },
+                    navigationIcon = {
+                        if (::navController.isInitialized && navController.previousBackStackEntry != null) {
+                            TextButton(onClick = { navController.navigateUp() }) {
+                                Text(text = "Back")
+                            }
+                        }
+                    },
+                )
+            }
 
-        setSupportActionBar(activityMainBinding.mainToolbar)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+            Box(modifier = Modifier.fillMaxSize()) {
+                AndroidView(
+                    modifier = Modifier.fillMaxSize(),
+                    factory = { context ->
+                        FragmentContainerView(context).apply {
+                            id = R.id.nav_host_fragment
+                            layoutParams = ViewGroup.LayoutParams(
+                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.MATCH_PARENT,
+                            )
 
-        navController.addOnDestinationChangedListener { item1, item2, item3 ->
-            if (item2.id == R.id.fragmentHome) {
-                activityMainBinding.mainToolbar.visibility = View.GONE
-            } else {
-                activityMainBinding.mainToolbar.visibility = View.VISIBLE
+                            val navHostFragment =
+                                supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
+                                    ?: NavHostFragment.create(R.navigation.nav_graph).also {
+                                        supportFragmentManager
+                                            .beginTransaction()
+                                            .replace(R.id.nav_host_fragment, it)
+                                            .setPrimaryNavigationFragment(it)
+                                            .commitNow()
+                                    }
 
+                            navController = navHostFragment.navController
+                            navController.addOnDestinationChangedListener { _, destination, _ ->
+                                showToolbar = destination.id != R.id.fragmentHome
+                            }
+                        }
+                    },
+                )
             }
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment).navigateUp()
+        return if (::navController.isInitialized) {
+            navController.navigateUp()
+        } else {
+            false
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
         OVERLAY_DISPLAYED = false
-
     }
 }
